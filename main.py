a50aa5066ca248fc322c06d6c061e4103679ac17
camilahurtado067-tech <camila.hurtado067@pascualbravo.edu.co>
2026-03-03 11:09:06 -0500

    agrega la función de los cajeros y la simulación final.

--- main.py
+++ main.py
@@ -46,3 +46,35 @@ if __name__ == "__main__":
 
     print(f"Saldo actual: {banco.saldo}")
     print(f"Clientes en espera: {cola_clientes.qsize()}")
+    
+    print("Cajeros activos: 3\n")
+
+# -----------------------
+# Función Cajero (Hilo)
+# -----------------------
+def cajero(id_cajero, banco, cola):
+    while not cola.empty():
+        cliente = cola.get()
+
+        print(f"[Cajero {id_cajero}] Atendiendo Cliente {cliente.id} - {cliente.tipo} ${cliente.monto}")
+
+        time.sleep(1)  # Simula tiempo de atención
+
+        if cliente.tipo == "deposito":
+            banco.depositar(cliente.monto)
+        else:
+            banco.retirar(cliente.monto)
+
+        cola.task_done()
+
+# Crear 3 cajeros
+hilos = []
+for i in range(3):
+    t = threading.Thread(target=cajero, args=(i+1, banco, cola_clientes))
+    hilos.append(t)
+    t.start()
+
+for t in hilos:
+    t.join()
+
+print("\nSaldo actual:", banco.saldo)
